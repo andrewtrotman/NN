d52384c19d62fb8a765eba28c3b39ee5e4599c24
Andrew Trotman <andrew@trotman.com>
2021-11-05 16:00:06 +1300

    more refactoring - removed node and replaced it with layer.

--- NN/nn.h
+++ NN/nn.h
@@ -1,19 +1,30 @@
 /*
 	NN.H
 	----
+	Copyright (c) 2021 Andrew Trotman
+	Released under the 2-clause BSD license (See:https://en.wikipedia.org/wiki/BSD_licenses)
+*/
+/*!
+	@file
+	@brief Simple multilayer preceptron neural network.
+	@author Andrew Trotman
+	@copyright 2021 Andrew Trotman
 */
 #pragma once
 
 #include <math.h>
 #include <algorithm>
 
-#include "nn.h"
-#include "node.h"
 #include "matrix.h"
 
 /*
 	RELU()
 	------
+*/
+/*!
+	@brief Rectified Linear Unit (ReLU)
+	@param input [in] The value to apply ReLU to.
+	@return 0 if input <= 0, else input.
 */
 inline double ReLU(double input)
 	{
@@ -24,6 +35,11 @@ inline double ReLU(double input)
 	RELU_DERIVERATIVE()
 	-------------------
 */
+/*!
+	@brief The derivitive of the Rectified Linear Unit (ReLU) functon
+	@param input [in] The value to apply ReLU to.
+	@return 1 if input > 0 else 0.
+*/
 inline double ReLU_derivitive(double input)
 	{
 	return input > 0 ? 1 : 0;
@@ -33,14 +49,75 @@ inline double ReLU_derivitive(double input)
 	CLASS NN
 	--------
 */
+/*!
+	@class nn
+	@brief Simple multilayer preceptron neural network.
+*/
 class nn
 	{
+	private:
+		/*
+			CLASS NN::LAYER
+			--------------
+		*/
+		/*!
+			@class layer
+			@brief A single layer in the multi-layer perceptron
+		*/
+		class layer
+			{
+			public:
+				matrix values;					// the values stored in each of the units at this level of the network
+				matrix weights_space;			// storage space for the current in-use set of weights ((switches using weights and weights_next)
+				matrix weights_next_space;		// storage space for the next set of weights (switches using weights and weights_next)
+				matrix *weights;				// the current set of weights
+				matrix *weights_next;			// the set of weights to use next iteration
+				matrix derivitive;				// the derivitive of the weights
+				matrix delta;					// the error at this level of the network
+
+			public:
+				/*
+					LAYER::LAYER()
+					--------------
+				*/
+				layer(matrix &training_data) :
+					values(training_data.rows, training_data.columns),
+					weights_space(0, 0),
+					weights_next_space(0, 0),
+					derivitive(0, 0),
+					delta(0,0)
+					{
+					weights = &weights_space;
+					weights_next = &weights_next_space;
+
+					values = training_data;
+					}
+
+				/*
+					LAYER::LAYER()
+					--------------
+				*/
+				layer(layer &previous, size_t units) :
+					values(previous.values.rows, units),
+					weights_space(previous.values.columns, units),
+					weights_next_space(previous.values.columns, units),
+					derivitive(previous.values.rows, units),
+					delta(previous.values.rows, units)
+					{
+					weights = &weights_space;
+					weights_next = &weights_next_space;
+
+					for (size_t entry = 0; entry < previous.values.columns * units; entry++)
+						weights->values[entry] = drand48();
+					}
+			};
+
 	private:
 		matrix training_data;
 		matrix training_answers;
 		double learning_parameter;
 
-		node **network;
+		layer **network;
 		size_t depth;
 
 	private:
@@ -56,8 +133,8 @@ class nn
 			training_answers(training_answers),
 			learning_parameter(0.001)
 			{
-			network = (node **)malloc(sizeof(*network));
-			network[0] = new node(training_data);
+			network = (layer **)malloc(sizeof(*network));
+			network[0] = new layer(training_data);
 			depth = 1;
 			}
 
@@ -76,8 +153,8 @@ class nn
 		*/
 		void add_layer(size_t units)
 			{
-			network = (node **)realloc(network, sizeof(*network) * (depth + 1));
-			network[depth] = new node(*network[depth - 1], units);
+			network = (layer **)realloc(network, sizeof(*network) * (depth + 1));
+			network[depth] = new layer(*network[depth - 1], units);
 			depth++;
 			}
 
